01da3d0c5154b76a4cecef8f393564293bae97a0
swaption2009 <swaption2009@gmail.com>
2016-04-23 11:59:26 -0700

    Lect 65

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,11 +5,16 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    if @article.save
+      flash[:notice] = "You've successfully added a new article"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
   end
 
   def show
+    @article = Article.find(params[:id])
   end
 
   private
