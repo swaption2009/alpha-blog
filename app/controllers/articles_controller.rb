9e8f28cfbe39cdefd1dac156c77d7ed69c38e428
swaption2009 <swaption2009@gmail.com>
2016-04-26 22:36:53 -0700

    flash and shared bootstrap

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "You've successfully added a new article"
+      flash[:success] = "You've successfully added a new article"
       redirect_to article_path(@article)
     else
       render 'new'
@@ -24,7 +24,7 @@ class ArticlesController < ApplicationController
 
   def update
     if @article.update(article_params)
-      flash[:notice] = "Article is succesfully updated"
+      flash[:success] = "Article is succesfully updated"
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -36,7 +36,7 @@ class ArticlesController < ApplicationController
 
   def destroy
     @article.destroy
-    flash[:notice] = "Article is succesfully deleted"
+    flash[:danger] = "Article is succesfully deleted"
     redirect_to articles_path
   end
 
