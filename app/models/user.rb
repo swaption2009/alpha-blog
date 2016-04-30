2ad9920176369c0b62144ca0b1cce3083c480eed
swaption2009 <swaption2009@gmail.com>
2016-04-29 20:27:42 -0700

    add bcrypt gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
   validates :username, presence: true, length: { minimum: 3, maximum: 25 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
 end
