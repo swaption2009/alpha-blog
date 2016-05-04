b945a12b0ebd2adfffb76619e45867c74ed37cb8
swaption2009 <swaption2009@gmail.com>
2016-05-03 23:12:07 -0700

    session create and destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  resources :comments
-
-  resources :users, except: [:new]
-  get 'signup', to: 'users#new'
   root 'pages#home'
-  get 'about', to: 'pages#about'
 
+  resources :comments
+  resources :users, except: [:new]
   resources :articles
+
+  get 'signup', to: 'users#new'
+  get 'about', to: 'pages#about'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
